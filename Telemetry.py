6bb83a55b0a631096d3c8dbca74f7bd82fba502b
Dkambich752 <davidkambich@gmail.com>
2026-03-20 12:15:47 -0600

    Induce bug for jenkins quality gate testing

--- Telemetry.py
+++ Telemetry.py
@@ -1,6 +1,6 @@
 #Telemetry App that measures signal power and reports strong or weak
 def checkSignalIntegrity(dBm):
-    if dBm > -120:
+    if dBm > 0:
         return "STRONG"
     else:
         return "WEAK"
