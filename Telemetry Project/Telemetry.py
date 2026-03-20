a83b767382b7946be4461a7979002d9b0ee7f5a4
Dkambich752 <davidkambich@gmail.com>
2026-03-20 10:32:38 -0600

    3rd commit to check if push is required to see on repo

--- Telemetry Project/Telemetry.py
+++ Telemetry Project/Telemetry.py
@@ -1,6 +1,6 @@
 #Telemetry App that measures signal power and reports strong or weak
 def checkSignalIntegrity(dBm):
-    if dBm > -80:
+    if dBm > 999:
         return "STRONG"
     else:
         return "WEAK"
