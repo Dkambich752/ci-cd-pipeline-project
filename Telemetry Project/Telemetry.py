119e55648af4d56450abf2a71a5b573cf66bf562
Dkambich752 <davidkambich@gmail.com>
2026-03-20 10:34:04 -0600

    Revert "3rd commit to check if push is required to see on repo" This is a revert commit test This reverts commit a83b767382b7946be4461a7979002d9b0ee7f5a4.

--- Telemetry Project/Telemetry.py
+++ Telemetry Project/Telemetry.py
@@ -1,6 +1,6 @@
 #Telemetry App that measures signal power and reports strong or weak
 def checkSignalIntegrity(dBm):
-    if dBm > 999:
+    if dBm > -80:
         return "STRONG"
     else:
         return "WEAK"
